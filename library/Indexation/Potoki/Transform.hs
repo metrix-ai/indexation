27906492cae269159e69c2d404cfd0f14f7d39cd
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-17 13:57:54 +0300

    Clean up

--- library/Indexation/Potoki/Transform.hs
+++ library/Indexation/Potoki/Transform.hs
@@ -10,14 +10,3 @@ import qualified STMContainers.Map as C
 
 indexConcurrently :: (Eq entity, Hashable entity) => Indexer entity -> Transform entity (Index entity)
 indexConcurrently indexer = Transform (return . A.indexConcurrently indexer)
-
-indexWithRef :: (Eq entity, Hashable entity) => IORef (IndexTable entity) -> Transform entity (Index entity)
-indexWithRef indexTableRef =
-  Transform $ \ fetch -> return (A.index indexTableRef fetch)
-
-{-|
-Index the entities emitting index and updated index table on each step.
--}
-indexWithState :: (Eq entity, Hashable entity) => Transform entity (Index entity, IndexTable entity)
-indexWithState =
-  runState (\ entity -> state (B.register entity)) B.empty
