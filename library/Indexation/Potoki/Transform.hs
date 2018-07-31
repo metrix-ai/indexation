f2a68d5f5826e9c8fef83628aa87d41a8e0caecd
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-31 15:42:57 +0300

    Add "lookup"

--- library/Indexation/Potoki/Transform.hs
+++ library/Indexation/Potoki/Transform.hs
@@ -3,15 +3,18 @@ module Indexation.Potoki.Transform
   Transform,
   Index(..),
   Indexer,
+  EntityTable,
   index,
+  lookup,
 )
 where
 
-import Indexation.Prelude hiding (runState)
+import Indexation.Prelude hiding (runState, index, lookup)
 import Indexation.Types
 import Potoki.Transform
 import qualified Focus
 import qualified StmContainers.Map as StmMap
+import qualified Data.Vector as Vector
 
 
 index :: (Eq entity, Hashable entity) => Indexer entity -> Transform entity (Index entity)
@@ -24,3 +27,9 @@ index (Indexer sizeVar map) =
         writeTVar sizeVar $! succ size
         return (Index size, Focus.Set size)
       reveal indexInt = return (Index indexInt, Focus.Leave)
+
+lookup :: EntityTable entity -> Transform (Index entity) (Maybe entity)
+lookup (EntityTable entityTableVector) =
+  arr $ \ (Index indexInt) -> if Vector.length entityTableVector > indexInt
+    then Just $! Vector.unsafeIndex entityTableVector indexInt
+    else Nothing
