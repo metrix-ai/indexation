1c75bbe8f234dd1da3173b944739bb4a127d7ca3
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-08 01:42:36 +0300

    Add "lookupEntity"

--- library/Indexation/Data.hs
+++ library/Indexation/Data.hs
@@ -3,8 +3,16 @@ module Indexation.Data
   Index(..),
   EntityTable,
   Indexer,
+  lookupEntity,
 )
 where
 
+import Indexation.Prelude
 import Indexation.Types
 import Indexation.Instances ()
+import qualified Data.Vector as Vector
+
+
+lookupEntity :: Index entity -> EntityTable entity -> Maybe entity
+lookupEntity (Index indexPrim) (EntityTable vector) =
+  vector Vector.!? indexPrim
