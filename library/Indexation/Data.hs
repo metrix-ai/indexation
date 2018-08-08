26e30bf3abb1bdbbdbf0a7e8502363a12e7dc5e3
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-08 17:49:18 +0300

    Add "lookupIndex"

--- library/Indexation/Data.hs
+++ library/Indexation/Data.hs
@@ -4,6 +4,7 @@ module Indexation.Data
   EntityTable,
   Indexer,
   lookupEntity,
+  lookupIndex,
 )
 where
 
@@ -11,8 +12,13 @@ import Indexation.Prelude
 import Indexation.Types
 import Indexation.Instances ()
 import qualified Data.Vector as Vector
+import qualified Data.HashMap.Strict as HashMap
 
 
 lookupEntity :: Index entity -> EntityTable entity -> Maybe entity
 lookupEntity (Index indexPrim) (EntityTable vector) =
   vector Vector.!? indexPrim
+
+lookupIndex :: (Eq entity, Hashable entity) => entity -> IndexTable entity -> Maybe (Index entity)
+lookupIndex entity (IndexTable _ hashMap) =
+  fmap Index (HashMap.lookup entity hashMap)
