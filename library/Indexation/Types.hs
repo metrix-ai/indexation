ebb51e26a45324e6e387a1b9eb59100f404f583f
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-12 03:59:01 +0300

    Switch to BasicHashTable to work around a bug in "hashtables", concerning uninitialized elements

--- library/Indexation/Types.hs
+++ library/Indexation/Types.hs
@@ -5,7 +5,7 @@ import Indexation.Prelude
 import qualified Data.HashTable.IO as HashtablesIO
 
 
-data Indexer entity = Indexer {-# UNPACK #-} !(IORef Int) {-# UNPACK #-} !(HashtablesIO.CuckooHashTable entity Int)
+data Indexer entity = Indexer {-# UNPACK #-} !(IORef Int) {-# UNPACK #-} !(HashtablesIO.BasicHashTable entity Int)
 
 data IndexTable entity = IndexTable {-# UNPACK #-} !Int {-# UNPACK #-} !(HashMap entity Int)
 
