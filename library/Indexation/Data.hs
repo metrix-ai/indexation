a7be0ae74ea620bb64f05950980bd3208afdfae0
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-28 23:32:46 +0300

    Add IndexSet

--- library/Indexation/Data.hs
+++ library/Indexation/Data.hs
@@ -4,8 +4,11 @@ module Indexation.Data
   EntityTable,
   IndexTable,
   Indexer,
+  IndexSet,
   lookupEntity,
   lookupIndex,
+  createIndexSet,
+  lookupInIndexSet,
 )
 where
 
@@ -14,6 +17,8 @@ import Indexation.Types
 import Indexation.Instances ()
 import qualified Data.Vector as Vector
 import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector.Unboxed as UnboxedVector
+import qualified Data.Vector.Unboxed.Mutable as MutableUnboxedVector
 
 
 lookupEntity :: Index entity -> EntityTable entity -> Maybe entity
@@ -23,3 +28,14 @@ lookupEntity (Index indexPrim) (EntityTable vector) =
 lookupIndex :: (Eq entity, Hashable entity) => entity -> IndexTable entity -> Maybe (Index entity)
 lookupIndex entity (IndexTable _ hashMap) =
   fmap Index (HashMap.lookup entity hashMap)
+
+createIndexSet :: (Eq entity, Hashable entity, Foldable foldable) => IndexTable entity -> foldable entity -> IndexSet entity
+createIndexSet (IndexTable size map) foldable = runST $ do
+  mv <- MutableUnboxedVector.new size
+  forM_ foldable $ \ entity -> forM_ (HashMap.lookup entity map) $ \ index -> do
+    MutableUnboxedVector.unsafeWrite mv index True
+  v <- UnboxedVector.unsafeFreeze mv
+  return (IndexSet v)
+
+lookupInIndexSet :: Index entity -> IndexSet entity -> Bool
+lookupInIndexSet (Index indexInt) (IndexSet vec) = vec UnboxedVector.!? indexInt & fromMaybe False
--- library/Indexation/Types.hs
+++ library/Indexation/Types.hs
@@ -3,6 +3,7 @@ where
 
 import Indexation.Prelude
 import qualified StmContainers.Map as StmMap
+import qualified Data.Vector.Unboxed as UnboxedVector
 
 
 data Indexer entity = Indexer {-# UNPACK #-} !(TVar Int) {-# UNPACK #-} !(StmMap.Map entity Int)
@@ -12,3 +13,9 @@ data IndexTable entity = IndexTable {-# UNPACK #-} !Int {-# UNPACK #-} !(HashMap
 newtype EntityTable entity = EntityTable (Vector entity)
 
 newtype Index entity = Index Int
+
+{-|
+Set of indices.
+A more efficient alternative to @HashSet (Index entity)@.
+-}
+newtype IndexSet entity = IndexSet (UnboxedVector.Vector Bool)
