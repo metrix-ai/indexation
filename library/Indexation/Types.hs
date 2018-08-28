849d0a628e663e1058548ffed25ba1aff1e838c0
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-28 23:02:47 +0300

    Switch to full aliases in Types

--- library/Indexation/Types.hs
+++ library/Indexation/Types.hs
@@ -2,10 +2,10 @@ module Indexation.Types
 where
 
 import Indexation.Prelude
-import qualified StmContainers.Map as A
+import qualified StmContainers.Map as StmMap
 
 
-data Indexer entity = Indexer {-# UNPACK #-} !(TVar Int) {-# UNPACK #-} !(A.Map entity Int)
+data Indexer entity = Indexer {-# UNPACK #-} !(TVar Int) {-# UNPACK #-} !(StmMap.Map entity Int)
 
 data IndexTable entity = IndexTable {-# UNPACK #-} !Int {-# UNPACK #-} !(HashMap entity Int)
 
