54581a3787dd5d0ed12e685449806289e5691592
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-09-09 13:15:37 +0300

    Add "counts" and "indexedCounts" produces

--- library/Indexation/Potoki/Produce.hs
+++ library/Indexation/Potoki/Produce.hs
@@ -1,11 +1,28 @@
 module Indexation.Potoki.Produce
 (
   entities,
-) where
+  counts,
+  indexedCounts,
+)
+where
 
 import Indexation.Prelude
 import Indexation.Types
 import qualified Potoki.Produce as Produce
+import qualified Data.Vector.Generic as GenericVector
+
 
 entities :: EntityTable a -> Produce a
 entities (EntityTable vector) = Produce.vector vector
+
+{-|
+Counts in the index-order.
+-}
+counts :: IndexCounts a -> Produce Word32
+counts (IndexCounts vector) = Produce.vector vector
+
+{-|
+Counts in the index-order paired with their indices.
+-}
+indexedCounts :: IndexCounts a -> Produce (Int, Word32)
+indexedCounts (IndexCounts vector) = Produce.vectorWithIndices vector
