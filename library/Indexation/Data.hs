99f7418a83d1514455954a2910bf63bd82d51a68
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-09-13 18:17:01 +0300

    Give up and expose the constructors

--- library/Indexation/Data.hs
+++ library/Indexation/Data.hs
@@ -4,6 +4,6 @@ module Indexation.Data
 )
 where
 
-import Indexation.Types as Exports (Index(..), EntityTable, IndexTable, ReindexTable, Indexer, IndexSet, IndexCounts)
+import Indexation.Types as Exports
 import Indexation.Instances ()
 import Indexation.Functions as Exports
