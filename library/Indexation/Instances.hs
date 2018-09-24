b88d05dc98339856bea6ad915885830119f4ff0b
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-09-24 14:02:44 +0300

    Add Unbox instances

--- library/Indexation/Instances.hs
+++ library/Indexation/Instances.hs
@@ -5,6 +5,8 @@ import Indexation.Prelude
 import Indexation.Types
 import Indexation.Instances.Cereal ()
 import Indexation.Functions
+import Data.Vector.Unboxed.Deriving (derivingUnbox)
+import Data.Bit (Bit)
 
 
 instance Show (Index a) where show (Index int) = show int
@@ -13,6 +15,15 @@ deriving instance Ord (Index a)
 deriving instance Hashable (Index a)
 deriving instance Generic (Index a)
 instance NFData (Index a)
+derivingUnbox "Index"
+  [t| forall a. Index a -> Int |]
+  [| \ (Index x) -> x |]
+  [| Index |]
+derivingUnbox "MaybeIndex"
+  [t| forall a. Maybe (Index a) -> (Bit, Index a) |]
+  [| maybe (0, Index 0) (\ x -> (1, x)) |]
+  [| \ case (1, x) -> Just x; _ -> Nothing |]
+
 
 instance Show (IndexSet a) where
   show = show . (\ (IndexSet x) -> x)
